ca2d3cb21873952d1c2b98fc8118afd3406edbc8
mercurystudio99 <mercurystudio99@gmail.com>
2023-06-17 23:44:06 -0700

    add carousel package

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,60 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
 import 'package:badges/badges.dart' as badges;
 import 'package:kalamazoo/utils/navigation_router.dart';
 import 'package:kalamazoo/utils/util.dart';
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          child: Container(
+            margin: EdgeInsets.all(5.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                child: Stack(
+                  children: <Widget>[
+                    Image.network(item, fit: BoxFit.cover, width: 1000.0),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(200, 0, 0, 0),
+                              Color.fromARGB(0, 0, 0, 0)
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        child: Text(
+                          'No. ${imgList.indexOf(item)} image',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ))
+    .toList();
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -64,6 +116,7 @@ class _HomeScreenState extends State<HomeScreen> {
     final List<Widget> widgetOptions = <Widget>[
       Column(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
         Container(
+          color: Colors.blue,
           padding: const EdgeInsets.only(top: 10.0),
           child: Column(children: [
             Padding(
@@ -82,6 +135,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           child: Icon(
                             value.visible ? Icons.clear : Icons.menu,
                             key: ValueKey<bool>(value.visible),
+                            color: Colors.white,
                           ),
                         );
                       },
@@ -90,7 +144,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      const Text('Location'),
+                      const Text(
+                        'Location',
+                        style: TextStyle(color: Colors.white),
+                      ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: const [
@@ -98,14 +155,20 @@ class _HomeScreenState extends State<HomeScreen> {
                             Icons.location_on,
                             color: Colors.red,
                           ),
-                          Text('Kalamazoo, Michigon'),
+                          Text(
+                            'Kalamazoo, Michigon',
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ],
                       )
                     ],
                   ),
                   const badges.Badge(
                     badgeContent: Text('1'),
-                    child: Icon(Icons.notifications_outlined),
+                    child: Icon(
+                      Icons.notifications_outlined,
+                      color: Colors.white,
+                    ),
                   ),
                 ],
               ),
@@ -136,17 +199,21 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const Text(Util.categories),
+                    const Text(
+                      Util.categories,
+                      style: TextStyle(color: Colors.white),
+                    ),
                     GestureDetector(
                       onTap: () {},
                       child: const Text(
                         Util.seeAll,
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
                   ]),
             ),
             SizedBox(
-                height: 100,
+                height: 50,
                 child: SingleChildScrollView(
                     scrollDirection: Axis.horizontal,
                     child: Row(
@@ -155,6 +222,277 @@ class _HomeScreenState extends State<HomeScreen> {
                       }).toList(),
                     ))),
           ]),
+        ),
+        Expanded(
+          child: ListView(
+            children: [
+              CarouselSlider(
+                options: CarouselOptions(
+                  aspectRatio: 2.0,
+                  enlargeCenterPage: true,
+                  pageViewKey: PageStorageKey<String>('carousel_slider'),
+                ),
+                items: imageSliders,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: Util.mainPadding),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(Util.homeTopBrands),
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          Util.seeAll,
+                        ),
+                      ),
+                    ]),
+              ),
+              SizedBox(
+                  height: 100,
+                  child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: countries.map((country) {
+                          return box(country, Colors.deepOrangeAccent);
+                        }).toList(),
+                      ))),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: Util.mainPadding),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(Util.homeBestOffers),
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          Util.seeAll,
+                        ),
+                      ),
+                    ]),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: Util.mainPadding),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: Card(
+                          shadowColor: Colors.blue,
+                          margin: const EdgeInsets.all(4.0),
+                          child: Column(
+                            children: [
+                              Image.asset('assets/group.png'),
+                              Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: const [
+                                        Text(
+                                          'Royal Din',
+                                          style: TextStyle(fontSize: 10.0),
+                                        ),
+                                        Text(
+                                          'Western dishes',
+                                          softWrap: true,
+                                          style: TextStyle(fontSize: 10.0),
+                                        ),
+                                      ],
+                                    ),
+                                    Container(
+                                      color: Colors.red,
+                                      child: Row(
+                                        children: const [
+                                          Text(
+                                            '5.3',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                          Icon(
+                                            Icons.star,
+                                            color: Colors.white,
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: const [
+                                        Text(
+                                          '50% OFF',
+                                          style: TextStyle(
+                                              fontSize: 10.0,
+                                              color: Colors.red,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          'UPTO \$100',
+                                          style: TextStyle(fontSize: 10.0),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: const [
+                                        Icon(
+                                          Icons.location_on,
+                                          color: Colors.red,
+                                          size: 10,
+                                        ),
+                                        Text(
+                                          '1.2km',
+                                          style: TextStyle(fontSize: 10.0),
+                                        ),
+                                        Icon(
+                                          Icons.access_time,
+                                          size: 10,
+                                        ),
+                                        Text(
+                                          '10min',
+                                          style: TextStyle(fontSize: 10.0),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: Card(
+                          shadowColor: Colors.blue,
+                          margin: const EdgeInsets.all(4.0),
+                          child: Column(
+                            children: [
+                              Image.asset('assets/group.png'),
+                              Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: const [
+                                        Text(
+                                          'Royal Din',
+                                          style: TextStyle(fontSize: 10.0),
+                                        ),
+                                        Text(
+                                          'Western dishes',
+                                          softWrap: true,
+                                          style: TextStyle(fontSize: 10.0),
+                                        ),
+                                      ],
+                                    ),
+                                    Container(
+                                      color: Colors.red,
+                                      child: Row(
+                                        children: const [
+                                          Text(
+                                            '5.3',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                          Icon(
+                                            Icons.star,
+                                            color: Colors.white,
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: const [
+                                        Text(
+                                          '50% OFF',
+                                          style: TextStyle(
+                                              fontSize: 10.0,
+                                              color: Colors.red,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          'UPTO \$100',
+                                          style: TextStyle(fontSize: 10.0),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: const [
+                                        Icon(
+                                          Icons.location_on,
+                                          color: Colors.red,
+                                          size: 10,
+                                        ),
+                                        Text(
+                                          '1.2km',
+                                          style: TextStyle(fontSize: 10.0),
+                                        ),
+                                        Icon(
+                                          Icons.access_time,
+                                          size: 10,
+                                        ),
+                                        Text(
+                                          '10min',
+                                          style: TextStyle(fontSize: 10.0),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+            ],
+          ),
         ),
       ]),
       Container(
