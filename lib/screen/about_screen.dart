6d46c7543b7a8425f45db7efe8f3b71e5329d449
mercurystudio99 <mercurystudio99@gmail.com>
2023-06-20 16:23:50 -0700

    modify about screen

--- lib/screen/about_screen.dart
+++ lib/screen/about_screen.dart
@@ -27,21 +27,66 @@ class _AboutScreenState extends State<AboutScreen> {
         children: <Widget>[
           Column(
             mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SvgPicture.asset('assets/background.svg'),
+              SvgPicture.asset(
+                'assets/background.svg',
+                width: MediaQuery.of(context).size.width,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.height * 0.25,
+                height: MediaQuery.of(context).size.height * 0.4,
+                decoration: BoxDecoration(
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: CustomColor.primaryColor.withOpacity(0.1),
+                      blurRadius: 30.0,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.zero,
+                      topRight:
+                          Radius.circular(MediaQuery.of(context).size.width),
+                      bottomLeft: Radius.zero,
+                      bottomRight:
+                          Radius.circular(MediaQuery.of(context).size.width)),
+                ),
+              ),
             ],
+          ),
+          Positioned(
+            left: 0,
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.height * 0.4,
+              height: MediaQuery.of(context).size.height * 0.4,
+              decoration: BoxDecoration(
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: CustomColor.primaryColor.withOpacity(0.1),
+                    blurRadius: 30.0,
+                  ),
+                ],
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.zero,
+                    topRight:
+                        Radius.circular(MediaQuery.of(context).size.width),
+                    bottomLeft: Radius.zero,
+                    bottomRight: Radius.zero),
+              ),
+            ),
           ),
           ListView(
             children: <Widget>[
               Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: Util.mainPadding),
+                padding: const EdgeInsets.symmetric(
+                    horizontal: Util.mainPadding * 0.5),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     IconButton(
-                      icon: const Icon(Icons.keyboard_arrow_left),
+                      icon: const Icon(Icons.arrow_back_ios),
                       onPressed: () {
                         NavigationRouter.back(context);
                       },
@@ -50,10 +95,12 @@ class _AboutScreenState extends State<AboutScreen> {
                       Util.about,
                       style: TextStyle(
                         color: CustomColor.primaryColor,
-                        fontSize: 20.0,
+                        fontSize: 22.0,
                       ),
                     ),
-                    const SizedBox.shrink(),
+                    const SizedBox(
+                      width: Util.mainPadding,
+                    ),
                   ],
                 ),
               ),
@@ -61,7 +108,7 @@ class _AboutScreenState extends State<AboutScreen> {
                 padding: const EdgeInsets.symmetric(
                     horizontal: Util.mainPadding, vertical: 10),
                 child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
+                  borderRadius: BorderRadius.circular(10),
                   child: Image.network(
                       'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80'),
                 ),
@@ -73,18 +120,27 @@ class _AboutScreenState extends State<AboutScreen> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text('Royal Dine Restaurant'),
+                    const Text(
+                      'Royal Dine Restaurant',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
                     Container(
-                      color: CustomColor.activeColor,
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
+                      decoration: BoxDecoration(
+                        color: CustomColor.activeColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                       child: Row(
                         children: const [
                           Text(
                             '5.3',
-                            style: TextStyle(color: Colors.white),
+                            style: TextStyle(color: Colors.white, fontSize: 12),
                           ),
                           Icon(
                             Icons.star,
                             color: Colors.white,
+                            size: 12,
                           )
                         ],
                       ),
@@ -99,6 +155,8 @@ class _AboutScreenState extends State<AboutScreen> {
                 leading: Container(
                   width: 50,
                   alignment: const Alignment(0, 0),
+                  decoration: const BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
                   child: const Icon(Icons.phone_in_talk_outlined),
                 ),
                 title: const Text(
@@ -107,7 +165,8 @@ class _AboutScreenState extends State<AboutScreen> {
                 ),
                 subtitle: const Text(
                   '0123456789',
-                  style: TextStyle(fontSize: 12.0),
+                  style: TextStyle(
+                      fontSize: 12.0, color: CustomColor.textDetailColor),
                 ),
               ),
               ListTile(
@@ -117,6 +176,8 @@ class _AboutScreenState extends State<AboutScreen> {
                 leading: Container(
                   width: 50,
                   alignment: const Alignment(0, 0),
+                  decoration: const BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
                   child: const Icon(Icons.email_outlined),
                 ),
                 title: const Text(
@@ -125,7 +186,8 @@ class _AboutScreenState extends State<AboutScreen> {
                 ),
                 subtitle: const Text(
                   'royaldine247@gmail.com',
-                  style: TextStyle(fontSize: 12.0),
+                  style: TextStyle(
+                      fontSize: 12.0, color: CustomColor.textDetailColor),
                 ),
               ),
               ListTile(
@@ -135,6 +197,8 @@ class _AboutScreenState extends State<AboutScreen> {
                 leading: Container(
                   width: 50,
                   alignment: const Alignment(0, 0),
+                  decoration: const BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
                   child: const Icon(Icons.location_on_outlined),
                 ),
                 trailing: TextButton(
@@ -149,7 +213,8 @@ class _AboutScreenState extends State<AboutScreen> {
                 ),
                 subtitle: const Text(
                   'Kalamazoo, Michigan, USA',
-                  style: TextStyle(fontSize: 12.0),
+                  style: TextStyle(
+                      fontSize: 12.0, color: CustomColor.textDetailColor),
                 ),
               ),
               Padding(
@@ -159,7 +224,11 @@ class _AboutScreenState extends State<AboutScreen> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text('Top Dishes'),
+                    const Text(
+                      'Top Dishes',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
                     TextButton(
                         onPressed: () {
                           NavigationRouter.switchToMenu(context);
@@ -172,7 +241,7 @@ class _AboutScreenState extends State<AboutScreen> {
                 ),
               ),
               SizedBox(
-                  height: 150,
+                  height: 160,
                   child: SingleChildScrollView(
                       scrollDirection: Axis.horizontal,
                       child: Row(
@@ -189,18 +258,25 @@ class _AboutScreenState extends State<AboutScreen> {
 
   Widget box(String title, Color backgroundcolor) {
     return Container(
-        margin: const EdgeInsets.all(10),
+        margin: const EdgeInsets.only(left: 8, right: 8, bottom: 20),
+        padding: const EdgeInsets.symmetric(vertical: 10),
         width: 150,
         decoration: BoxDecoration(
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: CustomColor.primaryColor.withOpacity(0.2),
+              blurRadius: 8,
+            ),
+          ],
           borderRadius: BorderRadius.circular(10),
           color: backgroundcolor,
         ),
         alignment: Alignment.center,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Image.asset('assets/group.png'),
-            Text(title, style: const TextStyle(fontSize: 20))
+            Text(title, style: const TextStyle(fontSize: 18))
           ],
         ));
   }
