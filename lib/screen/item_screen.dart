14880e0ab0bc880be002b16e4827a748600ffe1f
mercurystudio99 <mercurystudio99@gmail.com>
2023-06-20 17:56:22 -0700

    modify item screen

--- lib/screen/item_screen.dart
+++ lib/screen/item_screen.dart
@@ -24,14 +24,39 @@ class _ItemScreenState extends State<ItemScreen> {
                   'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80'),
             ],
           ),
-          Container(
-            padding: const EdgeInsets.fromLTRB(
-                Util.mainPadding, Util.mainPadding, Util.mainPadding, 0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
+          Positioned(
+            left: 0,
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.height * 0.4,
+              height: MediaQuery.of(context).size.height * 0.5,
+              decoration: BoxDecoration(
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: CustomColor.primaryColor.withOpacity(0.05),
+                    blurRadius: 30.0,
+                  ),
+                ],
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.zero,
+                    topRight:
+                        Radius.circular(MediaQuery.of(context).size.width),
+                    bottomLeft: Radius.zero,
+                    bottomRight: Radius.zero),
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: Util.mainPadding * 0.5, right: Util.mainPadding),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
@@ -47,93 +72,121 @@ class _ItemScreenState extends State<ItemScreen> {
                     const Icon(
                       Icons.bookmark,
                       color: CustomColor.activeColor,
+                      size: 30,
                     )
                   ],
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 10.0),
-                  alignment: Alignment.center,
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10.0),
+                alignment: Alignment.center,
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: MediaQuery.of(context).size.width / 2,
+                  padding: const EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border:
+                          Border.all(width: 4, color: CustomColor.activeColor),
+                      borderRadius: BorderRadius.all(Radius.circular(100))),
                   child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
+                    borderRadius: BorderRadius.all(Radius.circular(100)),
                     child: Image.asset(
                       'assets/group.png',
                       fit: BoxFit.cover,
-                      width: MediaQuery.of(context).size.width / 2,
-                      height: MediaQuery.of(context).size.width / 2,
                     ),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Card(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.remove_circle_outline),
-                            onPressed: () {},
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Card(
+                    shadowColor: CustomColor.primaryColor.withOpacity(0.2),
+                    elevation: 8,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.remove_circle_outline),
+                          onPressed: () {},
+                        ),
+                        const Text(
+                          '02',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        IconButton(
+                          icon: const Icon(
+                            Icons.add_circle,
+                            color: CustomColor.activeColor,
                           ),
-                          const Text('02'),
-                          IconButton(
-                            icon: const Icon(
-                              Icons.add_circle,
-                              color: CustomColor.activeColor,
-                            ),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 2.0, horizontal: Util.mainPadding),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Steak',
+                      style: TextStyle(
+                          color: CustomColor.primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                    Text(
+                      '\$120',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16.0),
                     ),
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const [
-                      Text(
-                        'Steak',
-                        style: TextStyle(
-                          color: CustomColor.primaryColor,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        '\$120',
-                        style: TextStyle(fontSize: 15.0),
-                      ),
-                    ],
-                  ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: 2.0, horizontal: Util.mainPadding),
+                child: Text(
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                  softWrap: true,
+                  style:
+                      TextStyle(height: 1.5, color: CustomColor.textHeadColor),
                 ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10.0),
-                  child: Text(
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-                    softWrap: true,
-                    style: TextStyle(height: 1.5),
-                  ),
-                ),
-                Row(
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: Util.mainPadding),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Container(
-                      color: CustomColor.activeColor,
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
+                      decoration: BoxDecoration(
+                        color: CustomColor.activeColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                       child: Row(
                         children: const [
                           Text(
                             '5.3',
-                            style: TextStyle(color: Colors.white),
+                            style: TextStyle(color: Colors.white, fontSize: 12),
                           ),
                           Icon(
                             Icons.star,
                             color: Colors.white,
+                            size: 12,
                           )
                         ],
                       ),
@@ -170,8 +223,8 @@ class _ItemScreenState extends State<ItemScreen> {
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+            ],
           )
         ],
       ),
