603ddfdfaa91c33a4a381adb0815394ec0e20aee
mercurystudio99 <mercurystudio99@gmail.com>
2023-07-06 06:26:47 -0700

    add new processing widget

---
+++ lib/widget/processing.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Processing extends StatelessWidget {
+  // Variables
+  final String? text;
+
+  const Processing({Key? key, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const MyCircularProgress(),
+          const SizedBox(height: 10),
+          Text(text ?? "Processing...", style: const TextStyle(fontSize: 18)),
+          const SizedBox(height: 5),
+          const Text("Please wait!", style: TextStyle(fontSize: 16)),
+        ],
+      ),
+    );
+  }
+}
+
+class MyCircularProgress extends StatelessWidget {
+  const MyCircularProgress({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(
+          valueColor:
+              AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor)),
+    );
+  }
+}
