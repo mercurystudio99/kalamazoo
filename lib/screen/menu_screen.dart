4b9a6c88cb9d436125b9d39533d40fef41b20e17
mercurystudio99 <mercurystudio99@gmail.com>
2023-07-06 23:29:50 -0700

    menu screen function

--- lib/screen/menu_screen.dart
+++ lib/screen/menu_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:kalamazoo/utils/util.dart';
 import 'package:kalamazoo/utils/navigation_router.dart';
 import 'package:kalamazoo/utils/color.dart';
+import 'package:kalamazoo/utils/constants.dart';
+import 'package:kalamazoo/models/app_model.dart';
+import 'package:kalamazoo/widget/processing.dart';
 
 class MenuScreen extends StatefulWidget {
   const MenuScreen({super.key});
@@ -12,141 +16,143 @@ class MenuScreen extends StatefulWidget {
 
 class _MenuScreenState extends State<MenuScreen> {
   bool isSelectionMode = false;
-  final int listLength = 30;
-  late List<bool> _selected;
 
   @override
   void initState() {
     super.initState();
-    initializeSelection();
-  }
-
-  void initializeSelection() {
-    _selected = List<bool>.generate(listLength, (_) => false);
   }
 
   @override
   void dispose() {
-    _selected.clear();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Positioned(
-            left: 0,
-            bottom: 0,
-            child: Container(
-              width: MediaQuery.of(context).size.height * 0.4,
-              height: MediaQuery.of(context).size.height * 0.4,
-              decoration: BoxDecoration(
-                boxShadow: <BoxShadow>[
-                  BoxShadow(
-                    color: CustomColor.primaryColor.withOpacity(0.1),
-                    blurRadius: 30.0,
-                  ),
-                ],
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.zero,
-                    topRight:
-                        Radius.circular(MediaQuery.of(context).size.width),
-                    bottomLeft: Radius.zero,
-                    bottomRight: Radius.zero),
-              ),
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
+        body: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+      future: AppModel().getMenu(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final List<QueryDocumentSnapshot<Map<String, dynamic>>> menu =
+              snapshot.data!.docs;
+          return Stack(
+            fit: StackFit.expand,
             children: <Widget>[
-              const SizedBox(
-                height: 20,
+              Positioned(
+                left: 0,
+                bottom: 0,
+                child: Container(
+                  width: MediaQuery.of(context).size.height * 0.4,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  decoration: BoxDecoration(
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: CustomColor.primaryColor.withOpacity(0.1),
+                        blurRadius: 30.0,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.zero,
+                        topRight:
+                            Radius.circular(MediaQuery.of(context).size.width),
+                        bottomLeft: Radius.zero,
+                        bottomRight: Radius.zero),
+                  ),
+                ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: Util.mainPadding * 0.5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.arrow_back_ios),
-                      onPressed: () {
-                        NavigationRouter.back(context);
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: Util.mainPadding * 0.5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.arrow_back_ios),
+                          onPressed: () {
+                            NavigationRouter.back(context);
+                          },
+                        ),
+                        const Text(
+                          Util.fullMenu,
+                          style: TextStyle(
+                            color: CustomColor.primaryColor,
+                            fontSize: 22.0,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: Util.mainPadding,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: Util.mainPadding,
+                        right: Util.mainPadding,
+                        bottom: 20),
+                    child: Material(
+                      borderRadius: const BorderRadius.all(Radius.circular(14)),
+                      elevation: 8,
+                      shadowColor: CustomColor.primaryColor.withOpacity(0.2),
+                      child: TextFormField(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Search Restaurant or Food...',
+                            prefixIconConstraints: BoxConstraints(
+                              minWidth: 50,
+                              minHeight: 2,
+                            ),
+                            prefixIcon: Icon(Icons.search_outlined, size: 24),
+                          ),
+                          autovalidateMode: AutovalidateMode.always,
+                          validator: (value) {
+                            if (value!.contains('\n')) {}
+                            return null;
+                          }),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListBuilder(
+                      isSelectionMode: isSelectionMode,
+                      list: menu,
+                      onSelectionChange: (bool x) {
+                        setState(() {
+                          isSelectionMode = x;
+                        });
                       },
                     ),
-                    const Text(
-                      Util.fullMenu,
-                      style: TextStyle(
-                        color: CustomColor.primaryColor,
-                        fontSize: 22.0,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: Util.mainPadding,
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: Util.mainPadding,
-                    right: Util.mainPadding,
-                    bottom: 20),
-                child: Material(
-                  borderRadius: const BorderRadius.all(Radius.circular(14)),
-                  elevation: 8,
-                  shadowColor: CustomColor.primaryColor.withOpacity(0.2),
-                  child: TextFormField(
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Search Restaurant or Food...',
-                        prefixIconConstraints: BoxConstraints(
-                          minWidth: 50,
-                          minHeight: 2,
-                        ),
-                        prefixIcon: Icon(Icons.search_outlined, size: 24),
-                      ),
-                      autovalidateMode: AutovalidateMode.always,
-                      validator: (value) {
-                        if (value!.contains('\n')) {}
-                        return null;
-                      }),
-                ),
-              ),
-              Expanded(
-                child: ListBuilder(
-                  isSelectionMode: isSelectionMode,
-                  selectedList: _selected,
-                  onSelectionChange: (bool x) {
-                    setState(() {
-                      isSelectionMode = x;
-                    });
-                  },
-                ),
-              ),
+                  ),
+                ],
+              )
             ],
-          )
-        ],
-      ),
-    );
+          );
+        } else {
+          return const Processing();
+        }
+      },
+    ));
   }
 }
 
 class ListBuilder extends StatefulWidget {
   const ListBuilder({
     super.key,
-    required this.selectedList,
+    required this.list,
     required this.isSelectionMode,
     required this.onSelectionChange,
   });
 
   final bool isSelectionMode;
-  final List<bool> selectedList;
+  final List<QueryDocumentSnapshot<Map<String, dynamic>>> list;
   final Function(bool)? onSelectionChange;
 
   @override
@@ -157,8 +163,9 @@ class _ListBuilderState extends State<ListBuilder> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: widget.selectedList.length,
+        itemCount: widget.list.length,
         itemBuilder: (_, int index) {
+          final Map<String, dynamic> menu = widget.list[index].data();
           return Padding(
               padding: const EdgeInsets.symmetric(horizontal: Util.mainPadding),
               child: Card(
@@ -177,7 +184,7 @@ class _ListBuilderState extends State<ListBuilder> {
                     contentPadding: const EdgeInsets.all(8),
                     leading: Image.asset('assets/group.png'),
                     title: Text(
-                      'Steak',
+                      menu[MENU_NAME],
                       style: TextStyle(
                           color: (widget.isSelectionMode
                               ? Colors.white
@@ -186,7 +193,7 @@ class _ListBuilderState extends State<ListBuilder> {
                           fontSize: 18.0),
                     ),
                     subtitle: Text(
-                      '\$120',
+                      '\$${menu[MENU_PRICE]}',
                       style: TextStyle(
                           overflow: TextOverflow.ellipsis,
                           fontSize: 12.0,
