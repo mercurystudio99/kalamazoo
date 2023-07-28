3496407238724516f2d7a142162b8f2528e3d45e
mercurystudio99 <mercurystudio99@gmail.com>
2023-07-28 01:48:34 -0700

    fix checkbox feature

--- lib/screen/registration_screen.dart
+++ lib/screen/registration_screen.dart
@@ -381,14 +381,14 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                       onPressed: () {
                         if (_isChecked) {
                           _checkConfirm = false;
+                          setState(() {});
                         } else {
                           _checkConfirm = true;
-                          return;
+                          setState(() {});
                         }
                         // Validate returns true if the form is valid, or false otherwise.
                         if (_formKey.currentState!.validate()) {
-                          // If the form is valid, display a snackbar. In the real world,
-                          // you'd often call a server or save the information in a database.
+                          if (!_isChecked) return;
                           ScaffoldMessenger.of(context).showSnackBar(
                             const SnackBar(content: Text('Processing Data')),
                           );
