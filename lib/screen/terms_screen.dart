59ae4e5243f825b80d1f4997a26450102d580efc
mercurystudio99 <mercurystudio99@gmail.com>
2023-09-10 19:54:26 -0700

    complete terms screen

--- lib/screen/terms_screen.dart
+++ lib/screen/terms_screen.dart
@@ -15,6 +15,57 @@ class TermsScreen extends StatefulWidget {
 class _TermsScreenState extends State<TermsScreen> {
   @override
   Widget build(BuildContext context) {
+    List<Widget> list = [
+      Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: Util.mainPadding * 0.5, vertical: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                NavigationRouter.back(context);
+              },
+            ),
+            const Text(
+              Util.termsTitle,
+              style: TextStyle(
+                color: CustomColor.primaryColor,
+                fontSize: 22.0,
+              ),
+            ),
+            const SizedBox(
+              width: Util.mainPadding,
+            ),
+          ],
+        ),
+      ),
+    ];
+    for (var element in Util.termsList) {
+      list.add(Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Util.mainPadding),
+        child: Text(
+          element['title'],
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+      ));
+      list.add(Padding(
+        padding: (element['title'].toString().isNotEmpty)
+            ? const EdgeInsets.symmetric(horizontal: Util.mainPadding)
+            : const EdgeInsets.only(
+                left: Util.mainPadding * 2, right: Util.mainPadding),
+        child: Text(
+          element['description'],
+          style: const TextStyle(fontSize: Util.descriptionSize),
+        ),
+      ));
+      list.add(const Padding(
+          padding: EdgeInsets.symmetric(horizontal: Util.mainPadding),
+          child: SizedBox(height: 30)));
+    }
+
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
@@ -30,35 +81,7 @@ class _TermsScreenState extends State<TermsScreen> {
             ],
           ),
           ListView(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: Util.mainPadding * 0.5),
-                  child: IconButton(
-                    icon: const Icon(
-                      Icons.arrow_back_ios,
-                      color: CustomColor.primaryColor,
-                    ),
-                    onPressed: () {
-                      NavigationRouter.back(context);
-                    },
-                  ),
-                ),
-              ),
-              const SizedBox(height: 80),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: Util.mainPadding),
-                child: Text(
-                  '',
-                  style: TextStyle(
-                      color: CustomColor.primaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: Util.titleSize),
-                ),
-              ),
-            ],
+            children: list,
           )
         ],
       ),
