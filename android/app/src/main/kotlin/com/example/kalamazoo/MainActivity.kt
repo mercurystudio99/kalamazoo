0db19ee5999db3507027cb88c4a54821a1024d47
mercurystudio99 <mercurystudio99@gmail.com>
2023-10-09 00:48:38 -0700

    temporarily

--- android/app/src/main/kotlin/com/example/kalamazoo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kalamazoo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kalamazoo
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
