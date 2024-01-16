dfe6ebd6bb3ffb4cf3db939eedd0e0bebdd8ec2b
mercurystudio99 <mercurystudio99@gmail.com>
2024-01-15 20:48:58 -0800

    add key's examples

---
+++ lib/key.example.dart
@@ -0,0 +1,5 @@
+const String kGoogleApiKey = "AIzaSyD8bMKeCR864zyCQmm4kB8ERsKDTfDG...";
+const String stripePublishableKey =
+    "pk_test_51Ng82wESItp7VBY7I9ww7yUea2A6Ul3iRsQMvSGuLyJjG337J6MtYxrjRvzRL7lhTU6Vrmz7ObdSGdNkiBFq7fsb00wu2dI...";
+const String stripeSecretKey =
+    "sk_test_51Ng82wESItp7VBY7x05wXpRKvqTBaYPVEHMSpAZLjZM8fzXl3PzjUqWG8LitrNob3rqZxlI8CUtMKKaiXiEH7qDS00wpXPA...";
